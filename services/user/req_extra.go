81a05b5fa6592f33a9928b2f5403fd2fdcc55245
xujing1123 <36979348+xujing1123@users.noreply.github.com>
2024-07-16 09:58:57 +0800

    Update req_extra.go

--- services/user/req_extra.go
+++ services/user/req_extra.go
@@ -20,7 +20,12 @@ import (
 func (r *AddReq) Check() (err error) {
 	return base.CheckAll(
 		func() (err error) { return base.CheckUsernameExists(r.Username) },
-		func() (err error) { return base.CheckRoleExist(r.RoleId) },
+		func() (err error) {
+			if r.RoleId > 0 {
+				return base.CheckRoleExist(r.RoleId)
+			}
+			return
+		},
 	)
 }
 
